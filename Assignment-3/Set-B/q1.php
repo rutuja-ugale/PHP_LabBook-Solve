79c17bb864daf30a6e9a679fbfe5aa11f62a6cfe
rutujaa121 <rutujaaugale@gmail.com>
2026-05-07 18:41:48 -0700

    add file q1.php

--- Assignment-3/Set-B/q1.php
+++ Assignment-3/Set-B/q1.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
     <form onsubmit="return validate()">
         <input type="text" placeholder="Username" id="t1"><br><br>
