ee58bdba143207d90b7b4aae3bc5a005f84a8eb0
rutujaa121 <rutujaaugale@gmail.com>
2026-05-07 18:37:57 -0700

    add file a1.php

---
+++ practice/a1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="a2.php" method="post">
+        Name: <input type="text" name="a1"><br><br>
+        Address: <input type="text" name="a2"><br><br>
+        Mobile: <input type="text" name="a3"><br><br>
+
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
+
