823da17d268cee35c3f3386eb23bb4a64e32bd36
rutujaa121 <rutujaaugale@gmail.com>
2026-05-13 22:04:09 -0700

    add file b1.php

--- practice/b1.php
+++ practice/b1.php
@@ -1,18 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
-    <form action="b2.php" method="POST">
-        Borrow No.: <input type="text" name="b1"><br><br>
-        Borrow Name: <input type="text" name="b2"><br><br>
-        Contact Number: <input type="text" name="b3"><br><br>
-        <input type="submit" value="submit">
+    <form action="b2.php" method="post">
+        Enter Name: <input type="text" name="b1"><br><br>
+        Number: <input type="text" name="b2"><br><br>
+        Address: <input type="text" name="b3"><br><br>
+        <input type="submit" value="Submit">
     </form>
 </body>
 </html>
