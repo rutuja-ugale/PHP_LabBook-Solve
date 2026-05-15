16f21f3100014b6aeb0f389e1e5e7c1f7c8416c6
rutujaa121 <rutujaaugale@gmail.com>
2026-05-14 22:35:02 -0700

    add file b2.php

--- practice/b2.php
+++ practice/b2.php
@@ -1,30 +1,26 @@
 <?php
-session_start();
-$bno = $_POST["b1"];
-$bname  = $_POST["b2"];
-$bnum = $_POST["b3"];
-$_SESSION['bno'] = $bno;
-$_SESSION['bname'] = $bname;
-$_SESSION['bnum'] = $bnum;
-?>
+    session_start();
+    $a = $_POST['b1'];
+    $b = $_POST['b2'];
+    $c = $_POST['b3'];
 
+    $_SESSION['b1']=$a;
+    $_SESSION['b2']=$b;
+    $_SESSION['b3']=$c;
+?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
-    <form action="b3.php" method="POST">
-        EMI NO. : <input type="text" name="t4"><br><br>
-        EMI AMOUNT : <input type="text" name="t5"><br><br>
-        <input type="submit" value="submit">
+    <form action="b3.php" method="post">
+        EMI Number <input type="text" name="e1"><br><br>
+        EMI Amount <input type="text" name="e2"><br><br>
+        <input type="submit" value="Submit">
     </form>
 </body>
 </html>
-
-
